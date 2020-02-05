e1237cc683b43ead9aada9bcc3d6f309efe5a791
rperea <r.perea.2019@alumnos.urjc.es>
2020-02-05 17:42:48 +0100

    just an other test...

--- Session-02/fibonacci.py
+++ Session-02/fibonacci.py
@@ -1,12 +1,9 @@
 #Write a program that prints on the console the first 11 terms of the Fibonacci series (staring from 0)
 
-
+n = 11
 a = 0
 b = 1
 
 for i in range(n):
-    c = b + a
-    a = b
-    b = c
-
-print(a)
+    print(a, end=' ')
+    a, b = b, a + b

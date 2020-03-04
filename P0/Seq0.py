8eafcf1a05d6447297523736a6a6a3802ba27ca0
raquelperea <r.perea.2019@alumnos.urjc.es>
2020-03-04 10:57:04 +0100

    commiting

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -7,14 +7,11 @@ def seq_ping():
 
 
 def seq_read_fasta(filename):
+    seq_dna = Path(filename).read_text()
+    seq_dna.split("\n")
 
-    file_contents = Path(filename).read_text()
-    seq_dna = file_contents
-    index_finish = seq_dna.find("\n")
-    seq_dna = seq_dna[index_finish + 1 ]
-    seq_dna = seq_dna.replace("\n", "")
-    seq_dna = seq_dna[:20]
     return seq_dna
 
 
-print(seq_read_fasta("U5.txt"))
+print(seq_read_fasta("../Session-04/U5.txt"))
+

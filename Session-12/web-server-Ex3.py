3da1b8ae819e31ab668ae8d2d854b300be5fc46f
raquelperea <r.perea.2019@alumnos.urjc.es>
2020-04-25 21:47:39 +0200

    Session-14

--- Session-12/web-server-Ex3.py
+++ Session-12/web-server-Ex3.py
@@ -51,7 +51,7 @@ def process_client(s):
     header = "Content-Type: text/html\n"
 
     # -- Add the Content-Length
-    header += f"Content-Length: {5}\n"
+    header += f"Content-Length: {len(body)}\n"
 
     # -- Build the message by joining together all the parts
     response_msg = status_line + header + "\n" + body
